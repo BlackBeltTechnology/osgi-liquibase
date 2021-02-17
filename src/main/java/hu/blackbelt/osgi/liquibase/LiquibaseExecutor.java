faa6ea17dbf2b20d5c8bd26a836daf6f0f1dcd78
Robert Csakany <csakany.robert@gmail.com>
2021-02-17 21:53:23 +0100

    Add support of changeset poperties

--- src/main/java/hu/blackbelt/osgi/liquibase/LiquibaseExecutor.java
+++ src/main/java/hu/blackbelt/osgi/liquibase/LiquibaseExecutor.java
@@ -4,6 +4,8 @@ import liquibase.exception.LiquibaseException;
 import org.osgi.framework.Bundle;
 
 import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
 
 public interface LiquibaseExecutor {
 
@@ -15,4 +17,15 @@ public interface LiquibaseExecutor {
      * @throws LiquibaseException
      */
     void executeLiquibaseScript(Connection connection, String scriptPathInBundle, Bundle scriptBundle) throws LiquibaseException;
+
+    /**
+     * Execute the given script with liquibase from the given bundle with given connection.
+     * @param connection
+     * @param scriptPathInBundle
+     * @param scriptBundle
+     * @param parameters
+     * @throws LiquibaseException
+     */
+    void executeLiquibaseScript(Connection connection, String scriptPathInBundle, Bundle scriptBundle, Map<String, Object> parameters) throws LiquibaseException;
+
 }
--- src/main/java/hu/blackbelt/osgi/liquibase/impl/DefaultLiquibaseExecutor.java
+++ src/main/java/hu/blackbelt/osgi/liquibase/impl/DefaultLiquibaseExecutor.java
@@ -12,6 +12,7 @@ import org.osgi.framework.Bundle;
 import org.osgi.service.component.annotations.Component;
 
 import java.sql.Connection;
+import java.util.Map;
 
 @Component(immediate = true)
 @Slf4j
@@ -19,13 +20,23 @@ public class DefaultLiquibaseExecutor implements LiquibaseExecutor {
 
     @Override
     public synchronized void executeLiquibaseScript(Connection connection, String scriptPathInBundle, Bundle scriptBundle) throws LiquibaseException {
+        executeLiquibaseScript(connection, scriptPathInBundle, scriptBundle, null);
+    }
+
+    @Override
+    public void executeLiquibaseScript(Connection connection, String scriptPathInBundle, Bundle scriptBundle, Map<String, Object> parameters) throws LiquibaseException {
         JdbcConnection jdbcConnection = new JdbcConnection(connection);
         Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
         database.setConnection(jdbcConnection);
 
-        Liquibase liquibase = new Liquibase(scriptPathInBundle,
+        final Liquibase liquibase = new Liquibase(scriptPathInBundle,
                 new BundleResourceAccessor(scriptBundle), database);
 
+        if (parameters != null) {
+            parameters.entrySet().stream().forEach(e ->
+                liquibase.setChangeLogParameter(e.getKey(), e.getValue())
+            );
+        }
         log.info("Processing: {}", scriptPathInBundle);
         liquibase.update((String) null);
     }
