099668e7ef165dda751aac631d3dc38de6b75d2d
Robert Csakany <csakany.robert@gmail.com>
2021-09-12 02:29:59 +0200

    JNG-2524 Upgrade liquibase to 4.4.3 (#4)

--- src/main/java/hu/blackbelt/osgi/liquibase/impl/DefaultLiquibaseExecutor.java
+++ src/main/java/hu/blackbelt/osgi/liquibase/impl/DefaultLiquibaseExecutor.java
@@ -2,21 +2,60 @@ package hu.blackbelt.osgi.liquibase.impl;
 
 import hu.blackbelt.osgi.liquibase.BundleResourceAccessor;
 import hu.blackbelt.osgi.liquibase.LiquibaseExecutor;
+import hu.blackbelt.osgi.liquibase.logging.Slf4jLogService;
 import liquibase.Liquibase;
+import liquibase.Scope;
+import liquibase.ScopeManager;
+import liquibase.SingletonScopeManager;
 import liquibase.database.Database;
 import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
+import liquibase.logging.LogFactory;
+import liquibase.logging.LogService;
+import liquibase.logging.core.LogServiceFactory;
+import liquibase.resource.CompositeResourceAccessor;
+import liquibase.resource.OSGiResourceAccessor;
+import liquibase.servicelocator.ServiceLocator;
 import lombok.extern.slf4j.Slf4j;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component(immediate = true)
 @Slf4j
 public class DefaultLiquibaseExecutor implements LiquibaseExecutor {
+
+    private String scopeId;
+    private BundleContext bundleContext;
+
+    @Activate
+    public void activate(BundleContext bundleContext) throws Exception {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
+        // Init
+        Scope.getCurrentScope();
+
+        Map<String, Object> scopeObjects = new HashMap<>();
+        scopeObjects.put(Scope.Attr.logService.name(), new Slf4jLogService());
+        scopeObjects.put(Scope.Attr.classLoader.name(), this.getClass().getClassLoader());
+        scopeId = Scope.enter(scopeObjects);
+
+        Thread.currentThread().setContextClassLoader(classLoader);
+        this.bundleContext = bundleContext;
+
+    }
+
+    @Deactivate
+    void deactivate() throws Exception {
+        Scope.exit(scopeId);
+    }
 
     @Override
     public synchronized void executeLiquibaseScript(Connection connection, String scriptPathInBundle, Bundle scriptBundle) throws LiquibaseException {
@@ -25,19 +64,32 @@ public class DefaultLiquibaseExecutor implements LiquibaseExecutor {
 
     @Override
     public void executeLiquibaseScript(Connection connection, String scriptPathInBundle, Bundle scriptBundle, Map<String, Object> parameters) throws LiquibaseException {
-        JdbcConnection jdbcConnection = new JdbcConnection(connection);
-        Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
-        database.setConnection(jdbcConnection);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(this.getClass().getClassLoader());
 
-        final Liquibase liquibase = new Liquibase(scriptPathInBundle,
-                new BundleResourceAccessor(scriptBundle), database);
+        try {
+            BundleResourceAccessor bundleResourceAccessor = new BundleResourceAccessor(scriptBundle);
+            JdbcConnection jdbcConnection = new JdbcConnection(connection);
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(jdbcConnection);
+            database.setConnection(jdbcConnection);
 
-        if (parameters != null) {
-            parameters.entrySet().stream().forEach(e ->
-                liquibase.setChangeLogParameter(e.getKey(), e.getValue())
-            );
+            final Liquibase liquibase = new Liquibase(scriptPathInBundle, bundleResourceAccessor, database);
+
+            if (parameters != null) {
+                parameters.entrySet().stream().forEach(e ->
+                        liquibase.setChangeLogParameter(e.getKey(), e.getValue())
+                );
+            }
+            log.info("Processing: {}", scriptPathInBundle);
+            liquibase.update((String) null);
+        } catch (LiquibaseException e) {
+            throw e;
+        } catch (Exception e) {
+        } finally {
+            try {
+                Thread.currentThread().setContextClassLoader(classLoader);
+            } catch (Exception e) {
+            }
         }
-        log.info("Processing: {}", scriptPathInBundle);
-        liquibase.update((String) null);
     }
 }
---
+++ src/main/java/hu/blackbelt/osgi/liquibase/logging/Slf4jLogger.java
@@ -0,0 +1,201 @@
+package hu.blackbelt.osgi.liquibase.logging;
+
+
+import liquibase.logging.LogMessageFilter;
+import liquibase.logging.core.AbstractLogger;
+import org.slf4j.Logger;
+
+import java.util.logging.Level;
+/**
+ * <p>An implementation of the Liquibase Logger that sends log output to SLF4J.</p>
+ *
+ * <p><strong>Log level Mappings:</strong>
+ *  <table summary="log level mappings">
+ *     <tr>
+ *         <th>Liquibase Level</th>
+ *         <th>SLF4J Level</th>
+ *     </tr>
+ *     <tr>
+ *         <td>Debug</td>
+ *         <td>Debug</td>
+ *     </tr>
+ *     <tr>
+ *         <td>Fine</td>
+ *         <td>Debug</td>
+ *     </tr>
+ *     <tr>
+ *         <td>Config</td>
+ *         <td>Info</td>
+ *     </tr>
+ *     <tr>
+ *         <td>Info</td>
+ *         <td>Info</td>
+ *     </tr>
+ *     <tr>
+ *         <td>Warning</td>
+ *         <td>Warn</td>
+ *     </tr>
+ *     <tr>
+ *         <td>Severe</td>
+ *         <td>Error</td>
+ *     </tr>
+ * </table>
+ *
+ * @see liquibase.logging.Logger
+ */
+public class Slf4jLogger extends AbstractLogger {
+
+    private static final int TRACE_THRESHOLD = Level.FINEST.intValue();
+    private static final int DEBUG_THRESHOLD = Level.FINE.intValue();
+    private static final int INFO_THRESHOLD = Level.INFO.intValue();
+    private static final int WARN_THRESHOLD = Level.WARNING.intValue();
+
+    private final Logger logger;
+
+    Slf4jLogger(Logger logger, LogMessageFilter filter) {
+        super(filter);
+        this.logger = logger;
+    }
+
+    @Override
+    public void log(Level level, String message, Throwable e) {
+        String filteredMessage = filterMessage(message);
+        int levelValue = level.intValue();
+        if (levelValue <= TRACE_THRESHOLD) {
+            logger.trace(filteredMessage, e);
+        } else if (levelValue <= DEBUG_THRESHOLD) {
+            logger.debug(filteredMessage, e);
+        } else if (levelValue <= INFO_THRESHOLD) {
+            logger.info(filteredMessage, e);
+        } else if (levelValue <= WARN_THRESHOLD) {
+            logger.warn(filteredMessage, e);
+        } else {
+            logger.error(filteredMessage, e);
+        }
+    }
+
+    /**
+     * Logs an severe message. Calls SLF4J {@link Logger#error(String)}.
+     *
+     * @param message The message to log.
+     */
+    @Override
+    public void severe(String message) {
+        if (logger.isErrorEnabled()) {
+            logger.error(filterMessage(message));
+        }
+    }
+
+    /**
+     * Logs a severe message. Calls SLF4J {@link Logger#error(String, Throwable)}.
+     *
+     * @param message The message to log
+     * @param e The exception to log.
+     */
+    @Override
+    public void severe(String message, Throwable e) {
+        if (logger.isErrorEnabled()) {
+            logger.error(filterMessage(message), e);
+        }
+    }
+
+    /**
+     * Logs a warning message. Calls SLF4J {@link Logger#warn(String)}
+     *
+     * @param message The message to log.
+     */
+    @Override
+    public void warning(String message) {
+        if (logger.isWarnEnabled()) {
+            logger.warn(filterMessage(message));
+        }
+    }
+
+    /**
+     * Logs a warning message. Calls SLF4J {@link Logger#warn(String, Throwable)}.
+     *
+     * @param message The message to log.
+     * @param e The exception to log.
+     */
+    @Override
+    public void warning(String message, Throwable e) {
+        if (logger.isWarnEnabled()) {
+            logger.warn(filterMessage(message), e);
+        }
+    }
+
+    /**
+     * Log an info message. Calls SLF4J {@link Logger#info(String)}.
+     *
+     * @param message The message to log.
+     */
+    @Override
+    public void info(String message) {
+        if (logger.isInfoEnabled()) {
+            logger.info(filterMessage(message));
+        }
+    }
+
+    /**
+     * Log an info message. Calls SLF4J {@link Logger#info(String, Throwable)}.
+     *
+     * @param message The message to log.
+     * @param e The exception to log.
+     */
+    @Override
+    public void info(String message, Throwable e) {
+        if (logger.isInfoEnabled()) {
+            logger.info(filterMessage(message), e);
+        }
+    }
+
+    /**
+     * Log a config message. Calls SLF4J {@link Logger#info(String)}.
+     *
+     * @param message The message to log.
+     */
+    @Override
+    public void config(String message) {
+        if (logger.isInfoEnabled()) {
+            logger.info(filterMessage(message));
+        }
+    }
+
+    /**
+     * Log a config message. Calls SLF4J {@link Logger#info(String, Throwable)}.
+     *
+     * @param message The message to log
+     * @param e The exception to log
+     */
+    @Override
+    public void config(String message, Throwable e) {
+        if (logger.isInfoEnabled()) {
+            logger.info(filterMessage(message), e);
+        }
+    }
+
+    /**
+     * Log a fine message. Calls SLF4J {@link Logger#debug(String)}.
+     *
+     * @param message The message to log.
+     */
+    @Override
+    public void fine(String message) {
+        if (logger.isDebugEnabled()) {
+            logger.debug(filterMessage(message));
+        }
+    }
+
+    /**
+     * Log a fine message. Calls SLF4J {@link Logger#debug(String, Throwable)}.
+     *
+     * @param message The message to log.
+     * @param e The exception to log.
+     */
+    @Override
+    public void fine(String message, Throwable e) {
+        if (logger.isDebugEnabled()) {
+            logger.debug(filterMessage(message), e);
+        }
+    }
+}
